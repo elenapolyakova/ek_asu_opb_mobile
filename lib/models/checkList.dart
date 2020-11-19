4c7cd29fb371aa1e8270be9fdf386e50b8b459a9
Igor Kovorotniy <kovorotniy@gmail.com>
2020-11-19 13:37:51 +0300

    Refactoring of checkList model

--- lib/models/checkList.dart
+++ lib/models/checkList.dart
@@ -1,18 +1,22 @@
+import 'dart:ffi';
+import 'dart:io';
+
 import "package:ek_asu_opb_mobile/models/models.dart";
 import 'package:ek_asu_opb_mobile/utils/convert.dart';
+import 'package:http/http.dart';
 
 class CheckListWork extends Models {
   int id;
   int odooId;
   // Plan id which uses this check list
   int parent_id;
-  bool is_base = false;
+  bool is_base;
   String name;
-  List child_ids;
+  String child_ids;
   int type;
   bool active = true;
   // Status of check list, true in work, false not used now
-  bool is_active = false;
+  bool is_active;
 
   CheckListWork({
     this.id,
@@ -30,12 +34,14 @@ class CheckListWork extends Models {
       new CheckListWork(
         id: json["id"],
         odooId: json["odooId"],
-        parent_id: json["parent_id"],
-        is_base: json["is_base"],
+        parent_id:
+            json["parent_id"] is Int32 ? getObj(json["parent_id"]) : null,
+        is_base: getObj(json["is_base"]),
         name: getStr(json["name"]),
         is_active: getObj(json["is_active"]),
         type: getObj(json["type"]),
-        child_ids: getObj(json["child_ids"]),
+        child_ids: getStr(json["child_ids"]),
+        // set false by default
         active: (json["active"].toString() == 'true'),
       );
 
@@ -51,5 +57,10 @@ class CheckListWork extends Models {
       'child_ids': child_ids,
       'is_active': (is_active == null || !is_active) ? 'false' : 'true',
     };
+  }
+
+  @override
+  String toString() {
+    return 'CheckListWork{id: $id, odooId: $odooId, parent_id: $parent_id, name: $name, type: $type, active: $active, is_base: $is_base, child_ids: $child_ids, is_active: $is_active }';
   }
 }
