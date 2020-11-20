d9bbeec8f8b0c58580f22c8f6f2d924fdeb57137
Igor Kovorotniy <kovorotniy@gmail.com>
2020-11-20 13:09:29 +0300

    Change logging for more information

--- lib/controllers/checkListItem.dart
+++ lib/controllers/checkListItem.dart
@@ -5,9 +5,23 @@ class CheckListItemController extends Controllers {
   static String _tableName = "check_list_item";
   static Future<dynamic> insert(Map<String, dynamic> json) async {
     CheckListItem checkListItem = CheckListItem.fromJson(json);
-    print("Check list item from json $checkListItem");
-    print("Check list item toJson()");
+    print("CheckListItem Insert() to db");
     print(checkListItem.toJson());
+
     return await DBProvider.db.insert(_tableName, checkListItem.toJson());
+  }
+
+  static Future<List<CheckListItem>> getQuestionsByParentId(
+      int parent_id) async {
+    var result = await DBProvider.db.executeQuery(
+        "SELECT * from check_list_item WHERE parent_id=$parent_id");
+    List<CheckListItem> response = [];
+
+    for (var item in result) {
+      var newItem = CheckListItem.fromJson(item);
+      response.add(newItem);
+    }
+    print("getQuestionsByParentId() response $response");
+    return response;
   }
 }
