5a8205b6a5870a8456b8f44dbb23b62ea1b9f496
Igor Kovorotniy <kovorotniy@gmail.com>
2020-11-20 18:20:11 +0300

    Add controller for updating is_active param in checkLists

--- lib/controllers/checkList.dart
+++ lib/controllers/checkList.dart
@@ -1,6 +1,7 @@
 import 'package:ek_asu_opb_mobile/controllers/checkListItem.dart';
 import "package:ek_asu_opb_mobile/controllers/controllers.dart";
 import "package:ek_asu_opb_mobile/models/checkList.dart";
+import 'package:ek_asu_opb_mobile/models/models.dart';
 import 'package:ek_asu_opb_mobile/utils/network.dart';
 
 class CheckListController extends Controllers {
@@ -83,5 +84,69 @@ class CheckListController extends Controllers {
     var dataToFront = await CheckListController.select(parentId);
 
     return dataToFront;
+  }
+
+  static Future<Map<String, dynamic>> setIsActiveTrue(
+      List ids, int parentId) async {
+    Map<String, dynamic> res = {
+      'code': null,
+      'message': null,
+      'id': null,
+    };
+
+    if (ids.length == 0) {
+      return {
+        "code": -1,
+        "message": "Не заданы id для обновления статуса!",
+        "id": null
+      };
+    }
+
+    var allCheckLists = await CheckListController.select(parentId);
+
+    if (allCheckLists.length == 0) {
+      res["code"] = -1;
+      res["message"] = "Не были найдены записи!";
+      res["id"] = null;
+
+      return res;
+    }
+
+    try {
+      var skipIdList = [];
+      for (var id in ids) {
+        print("Id $id");
+        for (var cList in allCheckLists) {
+          var json = cList.toJson();
+          var recordId = json["id"];
+
+          if (skipIdList.contains(json["id"])) {
+            continue;
+          } else {
+            if (id == json["id"]) {
+              await DBProvider.db.executeQuery(
+                  "UPDATE $_tableName SET is_active='true' WHERE id=$recordId");
+            } else {
+              await DBProvider.db.executeQuery(
+                  "UPDATE $_tableName SET is_active='false' WHERE id=$recordId");
+            }
+          }
+        }
+        skipIdList.add(id);
+      }
+
+      return {
+        "code": 1,
+        "message": "Successfully updates",
+        "id": 0,
+      };
+    } catch (e) {
+      print("setIsActiveTrue() Error: $e");
+      res["code"] = -3;
+      res["message"] = "Error updating table $_tableName, err: $e";
+      res["id"] = null;
+
+      return res;
+    }
   }
 }
