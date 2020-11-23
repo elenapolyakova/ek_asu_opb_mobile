962b49ad77c6ba3c1c910cfd6738313eed2e4cc4
Igor Kovorotniy <kovorotniy@gmail.com>
2020-11-23 14:35:06 +0300

    Extend checkListItem model by faultsCount param

--- lib/models/checkListItem.dart
+++ lib/models/checkListItem.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import "package:ek_asu_opb_mobile/models/models.dart";
 import 'package:ek_asu_opb_mobile/utils/convert.dart';
 
@@ -16,6 +14,8 @@ class CheckListItem extends Models {
   String result;
   String description;
   bool active = true;
+  // Count of faults(нарушений) для 1 вопроса
+  int faultsCount;
 
   CheckListItem({
     this.id,
@@ -27,6 +27,7 @@ class CheckListItem extends Models {
     this.result,
     this.description,
     this.active,
+    this.faultsCount,
   });
 
   factory CheckListItem.fromJson(Map<String, dynamic> json) =>
@@ -39,6 +40,7 @@ class CheckListItem extends Models {
           question: getStr(json["question"]),
           active: (json["active"].toString() == 'true'),
           result: getStr(json["result"]),
+          faultsCount: getObj(json["faultsCount"]),
           description: getStr(json["description"]));
 
   Map<String, dynamic> toJson() {
@@ -52,6 +54,7 @@ class CheckListItem extends Models {
       'active': (active == null || !active) ? 'false' : 'true',
       'description': description,
       'result': result,
+      'faultsCount': faultsCount,
     };
   }
 
@@ -65,6 +68,7 @@ class CheckListItem extends Models {
       'question': question,
       'description': description,
       'result': result,
+      'faultsCount': faultsCount,
     };
   }
 
