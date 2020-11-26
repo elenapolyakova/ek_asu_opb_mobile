9bde74db6bf09609e03f6a65fea8705c492bc30e
Igor Kovorotniy <kovorotniy@gmail.com>
2020-11-26 10:20:28 +0300

    Extend file_item table by new columns for interaction with odoo

--- lib/src/db.dart
+++ lib/src/db.dart
@@ -55,7 +55,7 @@ class DBProvider {
         await db.execute(
             "CREATE TABLE IF NOT EXISTS fault(id INTEGER PRIMARY KEY, odoo_id INTEGER, parent_id INTEGER, name TEXT, desc TEXT, fine_desc TEXT, fine INTEGER, koap_id INTEGER, date TEXT, date_done TEXT, desc_done TEXT, active TEXT, lat REAL, lon REAL, plan_fix_date TEXT)");
         await db.execute(
-            "CREATE TABLE IF NOT EXISTS fault_item(id INTEGER PRIMARY KEY, odoo_id INTEGER, parent_id INTEGER, image TEXT, active TEXT)");
+            "CREATE TABLE IF NOT EXISTS fault_item(id INTEGER PRIMARY KEY, odoo_id INTEGER, name STRING, type INTEGER, parent_id INTEGER, image TEXT, active TEXT, file_name STRING, file_data STRING)");
         await db.execute(
             "CREATE TABLE IF NOT EXISTS koap(id INTEGER PRIMARY KEY, article TEXT, paragraph TEXT, text TEXT, man_fine_from INTEGER, man_fine_to INTEGER, firm_fine_from INTEGER, firm_fine_to INTEGER, firm_stop INTEGER, desc TEXT, search_field TEXT)");
       },
