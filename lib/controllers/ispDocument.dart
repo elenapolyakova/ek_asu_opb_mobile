44f8e416b78a19faef9e12791df30e4108219b0e
Igor Kovorotniy <kovorotniy@gmail.com>
2020-11-28 19:53:01 +0300

    Add downloading document by request

--- lib/controllers/ispDocument.dart
+++ lib/controllers/ispDocument.dart
@@ -1,10 +1,18 @@
 import 'dart:async';
-
-import "package:ek_asu_opb_mobile/controllers/controllers.dart";
+import 'dart:io';
+import 'package:ek_asu_opb_mobile/controllers/controllers.dart';
 import 'package:ek_asu_opb_mobile/models/ispDocument.dart';
+import 'package:ek_asu_opb_mobile/utils/config.dart' as config;
+import 'package:ek_asu_opb_mobile/src/exchangeData.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:ek_asu_opb_mobile/src/fileStorage.dart';
 
 class ISPDocumentController extends Controllers {
   static String _tableName = "isp_document";
+
+  static final String baseURL =
+      '${config.getItem('ServiceRootUrl')}:${config.getItem('port')}';
+  static String documentModel = 'mob.document';
 
   static Future<dynamic> insert(Map<String, dynamic> json) async {
     ISPDocument nciDocument = ISPDocument.fromJson(json);
@@ -23,13 +31,13 @@ class ISPDocumentController extends Controllers {
 
     if (queryRes == null || queryRes.length == 0) return [];
 
-    List<ISPDocument> nciDocs =
+    List<ISPDocument> ispDocs =
         queryRes.map((e) => ISPDocument.fromJson(e)).toList();
 
     var res = await setIsNewFalse(parent2Id);
     print("Updates is_new state $res");
 
-    return nciDocs;
+    return ispDocs;
   }
 
   static Future<Map<String, dynamic>> setIsNewFalse(int parent2Id) async {
@@ -52,5 +60,42 @@ class ISPDocumentController extends Controllers {
     }
 
     return res;
+  }
+
+  static Future<Map<String, dynamic>> downloadDocument(
+      ISPDocument document) async {
+    final response = await getDataWithAttemp(documentModel, 'search_read', [
+      [
+        ['id', '=', document.id]
+      ],
+      [
+        'file_data',
+        'file_name',
+      ]
+    ], {});
+    return response[0];
+  }
+
+  static Future<File> getLocalDocument(ISPDocument document) async {
+    String path = document.file_path;
+    if (path == null || !(await File(path).exists())) {
+      Map<String, dynamic> record = await downloadDocument(document);
+      String appPath = (await getApplicationDocumentsDirectory()).path;
+      String newPath =
+          "$appPath/ispDocs/${DateTime.now().millisecondsSinceEpoch}/${record['file_name']}";
+      while (await File(newPath).exists()) {
+        newPath =
+            "$appPath/ispDocs/${DateTime.now().millisecondsSinceEpoch}/${record['file_name']}";
+      }
+      path = newPath;
+      await DBProvider.db
+          .update(_tableName, {'id': document.id, 'file_path': path});
+      await File(path).create(recursive: true);
+      return base64ToFile(
+        record['data'],
+        path: path,
+      );
+    }
+    return File(path);
   }
 }
