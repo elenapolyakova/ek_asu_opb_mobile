b720880b62498d893eb7f4128381eefe05cf90fd
Igor Kovorotniy <kovorotniy@gmail.com>
2020-11-28 17:41:07 +0300

    Refactoring

--- lib/controllers/nciDocument.dart
+++ lib/controllers/nciDocument.dart
@@ -1,7 +1,7 @@
 import "package:ek_asu_opb_mobile/controllers/controllers.dart";
 import 'package:ek_asu_opb_mobile/models/nciDocument.dart';
 
-class DocumentListController extends Controllers {
+class NCIDocumentController extends Controllers {
   static String _tableName = "nci_document";
 
   static Future<dynamic> insert(Map<String, dynamic> json) async {
