9c23f370952974df50b9ba8f6b44b31868060ec2
Igor Kovorotniy <kovorotniy@gmail.com>
2020-12-09 14:03:40 +0300

    Add model for FaultFix

---
+++ lib/models/faultFix.dart
@@ -0,0 +1,57 @@
+import "package:ek_asu_opb_mobile/models/models.dart";
+import 'package:ek_asu_opb_mobile/utils/convert.dart';
+
+class FaultFix extends Models {
+  // Model for control of execution of
+  int id;
+  int odoo_id;
+  // id of Fault
+  int parent_id;
+  // fault fix text description
+  String desc;
+  DateTime date;
+  // state
+  bool is_finished;
+  bool active;
+
+  FaultFix({
+    this.id,
+    this.odoo_id,
+    this.parent_id,
+    this.date,
+    this.desc,
+    this.active,
+    this.is_finished,
+  });
+
+  factory FaultFix.fromJson(Map<String, dynamic> json) => new FaultFix(
+      id: json["id"],
+      odoo_id: json["odoo_id"],
+      parent_id: json["parent_id"],
+      active: (json["active"].toString() == 'true'),
+      is_finished: (json["is_finished"].toString() == 'true'),
+      date: json["date"] == null ? null : DateTime.parse(json["date"]),
+      desc: json["desc"]);
+
+  Map<String, dynamic> toJson([omitId = false]) {
+    Map<String, dynamic> res = {
+      'id': id,
+      'odoo_id': odoo_id,
+      'parent_id': parent_id,
+      'active': (active == null || !active) ? 'false' : 'true',
+      'is_finished': (is_finished == null || !is_finished) ? 'false' : 'true',
+      'desc': desc,
+      'date': dateTimeToString(date),
+    };
+    if (omitId) {
+      res.remove("id");
+      res.remove("odoo_id");
+    }
+    return res;
+  }
+
+  @override
+  String toString() {
+    return 'FaultFix {id: $id, odooId: $odoo_id, parent_id: $parent_id, active: $active }';
+  }
+}
