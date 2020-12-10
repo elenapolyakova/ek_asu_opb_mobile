ae3b157963709eddb2011be37886e7e7970d4fa5
leaverock <minor3994@rambler.ru>
2020-12-10 22:07:53 +0300

    Add paging(100 per request) while loading department

--- lib/src/exchangeData.dart
+++ lib/src/exchangeData.dart
@@ -48,271 +48,278 @@ Future<List<Map<String, dynamic>>> getDictionaries(
     try {
       LogController.insert(dicts[i]);
       dynamic lastUpdate = isLastUpdate ? await getLastUpdate(dicts[i]) : null;
-      switch (dicts[i]) {
-        case 'railway':
-          List<dynamic> domain = new List<dynamic>();
-          if (lastUpdate != null) domain.add(lastUpdate);
-          data =
-              await getDataWithAttemp('eco.ref.railway', 'search_read', null, {
-            'domain': domain,
-            'fields': ['id', 'name', 'short_name']
-          });
+      int page = 0;
+      do {
+        print('Downloading ${dicts[i]}. Page $page.');
+        switch (dicts[i]) {
+          case 'railway':
+            List<dynamic> domain = new List<dynamic>();
+            if (lastUpdate != null) domain.add(lastUpdate);
+            data = await getDataWithAttemp(
+                'eco.ref.railway', 'search_read', null, {
+              'domain': domain,
+              'fields': ['id', 'name', 'short_name']
+            });
 
-          break;
-        case 'koap':
-          List<dynamic> domain = new List<dynamic>();
-          if (lastUpdate != null) domain.add(lastUpdate);
-          data = await getDataWithAttemp('mob.ref.koap', 'search_read', null, {
-            'domain': domain,
-            'fields': [
-              'id',
-              'article',
-              'paragraph',
-              'text',
-              'man_fine_from',
-              'man_fine_to',
-              'firm_fine_from',
-              'firm_fine_to',
-              'firm_stop',
-              'desc',
-            ]
-          });
+            break;
+          case 'koap':
+            List<dynamic> domain = new List<dynamic>();
+            if (lastUpdate != null) domain.add(lastUpdate);
+            data =
+                await getDataWithAttemp('mob.ref.koap', 'search_read', null, {
+              'domain': domain,
+              'fields': [
+                'id',
+                'article',
+                'paragraph',
+                'text',
+                'man_fine_from',
+                'man_fine_to',
+                'firm_fine_from',
+                'firm_fine_to',
+                'firm_stop',
+                'desc',
+              ]
+            });
 
-          break;
-        case 'department':
-          List<dynamic> domain = new List<dynamic>();
-          if (lastUpdate != null) domain.add(lastUpdate);
-         /* domain.add([
+            break;
+          case 'department':
+            List<dynamic> domain = new List<dynamic>();
+            if (lastUpdate != null) domain.add(lastUpdate);
+            /* domain.add([
             'id',
             'in',
             [32229, 32230, 22886, 21818]
           ]);*/
-          data =
-              await getDataWithAttemp('eco.department', 'search_read', null, {
-            'domain': domain,
-            'fields': [
-              'id',
-              'name',
-              'short_name',
-              'rel_railway_id',
-              'active',
-              'f_inn',
-              'f_ogrn',
-              'f_okpo',
-              'f_addr',
-              'director_fio',
-              'director_email',
-              'director_phone',
-              'deputy_fio',
-              'deputy_email',
-              'deputy_phone',
-              'rel_sector_id',
-              'f_coord_n',
-              'f_coord_e'
-            ],
-            // 'limit': 100,
-          });
+            data =
+                await getDataWithAttemp('eco.department', 'search_read', null, {
+              'domain': domain,
+              'fields': [
+                'id',
+                'name',
+                'short_name',
+                'rel_railway_id',
+                'active',
+                'f_inn',
+                'f_ogrn',
+                'f_okpo',
+                'f_addr',
+                'director_fio',
+                'director_email',
+                'director_phone',
+                'deputy_fio',
+                'deputy_email',
+                'deputy_phone',
+                'rel_sector_id',
+                'f_coord_n',
+                'f_coord_e'
+              ],
+              'limit': 100,
+              'offset': 100 * page++,
+            });
 
-          break;
-        case 'user':
-          List<dynamic> domain = new List<dynamic>();
+            break;
+          case 'user':
+            List<dynamic> domain = new List<dynamic>();
 
-          if (lastUpdate != null) domain.add(lastUpdate);
-          
-          //раскоментировать, если нужно будет огграничивать пользователей дорогой нцоп
-          /*listDepIds = await DepartmentController.selectIDs();
+            if (lastUpdate != null) domain.add(lastUpdate);
+
+            //раскоментировать, если нужно будет огграничивать пользователей дорогой нцоп
+            /*listDepIds = await DepartmentController.selectIDs();
           if (listDepIds == null) continue;
           if (listDepIds.length > 0) if (userRoleTxt == ncopRole)
             domain.add(['department_id', 'in', listDepIds]);*/
 
-
-          // domain.add(['department_id.role', 'in', ['cbt', 'ncop']]);
-          /* domain.add([
+            // domain.add(['department_id.role', 'in', ['cbt', 'ncop']]);
+            /* domain.add([
             'f_user_role_txt',
             'in',
             [cbtRole, ncopRole]
           ]);*/
-          domain.add([
-            'user_role',
-            'in',
-            ['cbt', 'ncop']
-          ]);
-          data = await getDataWithAttemp('res.users', 'search_read', null, {
-            'domain': domain,
-            'fields': [
-              'id',
-              'login',
-              'f_user_role_txt',
-              'display_name',
-              'department_id',
-              'rel_railway_id',
-              'email',
-              'phone',
-              'active',
-              'function',
-              'user_role'
-            ]
-          });
+            domain.add([
+              'user_role',
+              'in',
+              ['cbt', 'ncop']
+            ]);
+            data = await getDataWithAttemp('res.users', 'search_read', null, {
+              'domain': domain,
+              'fields': [
+                'id',
+                'login',
+                'f_user_role_txt',
+                'display_name',
+                'department_id',
+                'rel_railway_id',
+                'email',
+                'phone',
+                'active',
+                'function',
+                'user_role'
+              ]
+            });
 
-          break;
-        case 'check_list':
-          List<dynamic> domain = new List<dynamic>();
-          if (lastUpdate != null) domain.add(lastUpdate);
+            break;
+          case 'check_list':
+            List<dynamic> domain = new List<dynamic>();
+            if (lastUpdate != null) domain.add(lastUpdate);
 
-          domain.add(['parent_id', '=', null]);
-          domain.add(['is_base', '=', true]);
-          data =
-              await getDataWithAttemp('mob.check.list', 'search_read', null, {
-            'domain': domain,
-            'fields': [
-              'id',
-              'parent_id',
-              'is_active',
-              'base_id',
-              'name',
-              'type',
-              'active',
-              'type',
-              'is_base',
-              'child_ids'
-            ]
-          });
-          if (data.length > 0) {
-            for (var element in data) {
-              var assignedQuestions = await getDataWithAttemp(
-                  'mob.check.list.item', 'search_read', null, {
-                'domain': [
-                  ['id', 'in', element["child_ids"]]
-                ],
-                'fields': [
-                  'id',
-                  'parent_id',
-                  'name',
-                  'question',
-                  'result',
-                  'description',
-                  'active',
-                  'base_id',
-                ]
-              });
+            domain.add(['parent_id', '=', null]);
+            domain.add(['is_base', '=', true]);
+            data =
+                await getDataWithAttemp('mob.check.list', 'search_read', null, {
+              'domain': domain,
+              'fields': [
+                'id',
+                'parent_id',
+                'is_active',
+                'base_id',
+                'name',
+                'type',
+                'active',
+                'type',
+                'is_base',
+                'child_ids'
+              ]
+            });
+            if (data.length > 0) {
+              for (var element in data) {
+                var assignedQuestions = await getDataWithAttemp(
+                    'mob.check.list.item', 'search_read', null, {
+                  'domain': [
+                    ['id', 'in', element["child_ids"]]
+                  ],
+                  'fields': [
+                    'id',
+                    'parent_id',
+                    'name',
+                    'question',
+                    'result',
+                    'description',
+                    'active',
+                    'base_id',
+                  ]
+                });
 
-              for (var q in assignedQuestions) {
-                //  As we perform first download from odoo, we set odooId as id of input
-                q["odoo_id"] = q["id"];
-                // If parent id exists like [3, someName]
-                if (q["parent_id"] is List) {
-                  var parent_id = q["parent_id"][0];
-                  q["parent_id"] = parent_id;
-                } else {
-                  q["parent_id"] = null;
-                }
-              }
-
-              element["q_data"] = assignedQuestions;
-              // Set odooId as main ID for check list, as it's first download
-              element["odoo_id"] = element["id"];
-            }
-          }
-          break;
-        case 'isp':
-          List<dynamic> domain = new List<dynamic>();
-          if (lastUpdate != null) domain.add(lastUpdate);
-
-          data = await getDataWithAttemp(
-              'mob.document_list', 'search_read', null, {
-            'domain': domain,
-            'fields': ['id', 'parent_id', 'name']
-          });
-
-          if (data.length > 0) {
-            for (var docList in data) {
-              // Это вложение
-              if (docList["parent_id"] is List) {
-                docList['parent_id'] = docList['parent_id'][0];
-              }
-
-              domain.add(['parent2_id', '=', docList["id"]]);
-
-              var assignedDocs =
-                  await getDataWithAttemp('mob.document', 'search_read', null, {
-                'domain': domain,
-                'fields': [
-                  'id',
-                  'parent2_id',
-                  'name',
-                  'file_name',
-                  'type',
-                  'number',
-                  'description'
-                ]
-              });
-              docList["docs"] = assignedDocs;
-
-              // Update domain for new docListInputs
-              domain = [];
-              // IF not null get only last added docs!
-              if (lastUpdate != null) domain.add(lastUpdate);
-            }
-          }
-          break;
-      } //switch
-      if (data != null) {
-        List<dynamic> dataList = data as List<dynamic>;
-        for (int j = 0; j < dataList.length; j++) {
-          switch (dicts[i]) {
-            case 'railway':
-              await RailwayController.insert(
-                  dataList[j] as Map<String, dynamic>);
-              break;
-            case 'koap':
-              await KoapController.insert(dataList[j] as Map<String, dynamic>);
-              break;
-            case 'department':
-              await DepartmentController.insert(
-                  dataList[j] as Map<String, dynamic>);
-              break;
-            case 'user':
-              await UserController.insert(dataList[j] as Map<String, dynamic>);
-              break;
-            case 'check_list':
-              await CheckListController.insert(
-                  dataList[j] as Map<String, dynamic>);
-              if (dataList[j]["q_data"].length > 0) {
-                for (var payload in dataList[j]["q_data"]) {
-                  await CheckListItemController.insert(
-                      payload as Map<String, dynamic>);
-                }
-              }
-              break;
-            case 'isp':
-              await DocumentListController.insert(
-                  dataList[j] as Map<String, dynamic>);
-
-              if (dataList[j]["docs"].length > 0) {
-                for (var doc in dataList[j]["docs"]) {
-                  if (doc['parent2_id'] is List) {
-                    doc['parent2_id'] = doc['parent2_id'][0];
+                for (var q in assignedQuestions) {
+                  //  As we perform first download from odoo, we set odooId as id of input
+                  q["odoo_id"] = q["id"];
+                  // If parent id exists like [3, someName]
+                  if (q["parent_id"] is List) {
+                    var parent_id = q["parent_id"][0];
+                    q["parent_id"] = parent_id;
+                  } else {
+                    q["parent_id"] = null;
                   }
-                  // Important!
-                  doc['is_new'] = true;
-                  await ISPDocumentController.insert(
-                      doc as Map<String, dynamic>);
                 }
+
+                element["q_data"] = assignedQuestions;
+                // Set odooId as main ID for check list, as it's first download
+                element["odoo_id"] = element["id"];
               }
-          } //switch
-        } //for j
+            }
+            break;
+          case 'isp':
+            List<dynamic> domain = new List<dynamic>();
+            if (lastUpdate != null) domain.add(lastUpdate);
 
-        var recordCount = (data as List<dynamic>).length.toString();
-        print('Recived $recordCount records');
-        LogController.insert('Recived $recordCount records');
-        LogController.insert("----------------------------------------");
+            data = await getDataWithAttemp(
+                'mob.document_list', 'search_read', null, {
+              'domain': domain,
+              'fields': ['id', 'parent_id', 'name']
+            });
 
-        result.add({
-          dicts[i]: [1, recordCount]
-        });
+            if (data.length > 0) {
+              for (var docList in data) {
+                // Это вложение
+                if (docList["parent_id"] is List) {
+                  docList['parent_id'] = docList['parent_id'][0];
+                }
 
-        await setLastUpdate(dicts[i]);
-      } //if (data!=null)
+                domain.add(['parent2_id', '=', docList["id"]]);
+
+                var assignedDocs = await getDataWithAttemp(
+                    'mob.document', 'search_read', null, {
+                  'domain': domain,
+                  'fields': [
+                    'id',
+                    'parent2_id',
+                    'name',
+                    'file_name',
+                    'type',
+                    'number',
+                    'description'
+                  ]
+                });
+                docList["docs"] = assignedDocs;
+
+                // Update domain for new docListInputs
+                domain = [];
+                // IF not null get only last added docs!
+                if (lastUpdate != null) domain.add(lastUpdate);
+              }
+            }
+            break;
+        } //switch
+        if (data != null) {
+          List<dynamic> dataList = data as List<dynamic>;
+          for (int j = 0; j < dataList.length; j++) {
+            switch (dicts[i]) {
+              case 'railway':
+                await RailwayController.insert(
+                    dataList[j] as Map<String, dynamic>);
+                break;
+              case 'koap':
+                await KoapController.insert(
+                    dataList[j] as Map<String, dynamic>);
+                break;
+              case 'department':
+                await DepartmentController.insert(
+                    dataList[j] as Map<String, dynamic>);
+                break;
+              case 'user':
+                await UserController.insert(
+                    dataList[j] as Map<String, dynamic>);
+                break;
+              case 'check_list':
+                await CheckListController.insert(
+                    dataList[j] as Map<String, dynamic>);
+                if (dataList[j]["q_data"].length > 0) {
+                  for (var payload in dataList[j]["q_data"]) {
+                    await CheckListItemController.insert(
+                        payload as Map<String, dynamic>);
+                  }
+                }
+                break;
+              case 'isp':
+                await DocumentListController.insert(
+                    dataList[j] as Map<String, dynamic>);
+
+                if (dataList[j]["docs"].length > 0) {
+                  for (var doc in dataList[j]["docs"]) {
+                    if (doc['parent2_id'] is List) {
+                      doc['parent2_id'] = doc['parent2_id'][0];
+                    }
+                    // Important!
+                    doc['is_new'] = true;
+                    await ISPDocumentController.insert(
+                        doc as Map<String, dynamic>);
+                  }
+                }
+            } //switch
+          } //for j
+
+          var recordCount = (data as List<dynamic>).length.toString();
+          print('Recived $recordCount records');
+          LogController.insert('Recived $recordCount records');
+          LogController.insert("----------------------------------------");
+
+          result.add({
+            dicts[i]: [1, recordCount]
+          });
+
+          await setLastUpdate(dicts[i]);
+        } //if (data!=null)
+      } while (data is List && data.length == 100);
     } //try
     catch (e) {
       print(e);
